2e05198c88dcdc3b9315f0886c0351ba72fe0df6
Thierry Onkelinx <ThierryO@users.noreply.github.com>
2022-10-04 20:34:26 +0200

    add overview of training progress

--- app.R
+++ app.R
@@ -7,38 +7,8 @@ library(digest)
 library(shiny)
 library(INLA)
 
-lees_vertalingen <- function(
-  sheet = "1w85ll_qVEPkybDEPPovji_bmLAS5TXXFNqMRGOY0TTs",
-  taal = "nederlands-frans"
-) {
-  read_sheet(sheet, taal) %>%
-    mutate(
-      hash = pmap_chr(
-        list(
-          type = .data$type, werkwoord = .data$werkwoord,
-          persoon = .data$persoon, werkwoordstijd = .data$werkwoordstijd
-        ),
-        function(...) {
-          sha1(list(...))
-        }
-      ),
-      type = factor(.data$type),
-      werkwoord = factor(.data$werkwoord),
-      persoon = factor(.data$persoon),
-      werkwoordstijd = factor(.data$werkwoordstijd)
-    )
-}
-lees_antwoorden <- function(taal = "nederlands-frans") {
-  if(!is_git2rmeta(taal)) {
-    return(
-      data.frame(
-        hash = character(0), tijdstip = as.POSIXct(integer(0)),
-        juist = integer(0), fout = character(0)
-      )
-    )
-  }
-  read_vc(taal)
-}
+source("helper.R")
+
 selectiekans_bijwerken <- function(vragen, antwoord) {
   if ((sum(antwoord$juist) < 20) || (sum(1 - antwoord$juist) < 20)) {
     vragen %>%
