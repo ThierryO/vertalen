ed491f4d6830cc7572aae9b73b2cf98969afc804
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2023-09-14 17:20:54 +0200

    add tool to translate words

--- app.R
+++ app.R
@@ -10,15 +10,13 @@ library(scales)
 
 source("helper.R")
 
-selectiekans_bijwerken <- function(vragen, antwoord) {
+selectiekans_bijwerken <- function(vragen, antwoord, type = "vervoeging") {
   if ((sum(antwoord$juist) < 20) || (sum(1 - antwoord$juist) < 20)) {
-    vragen %>%
-      left_join(antwoord, by = "hash") %>%
-      group_by(
-        .data$type, .data$werkwoord, .data$persoon, .data$werkwoordstijd,
-        .data$nederlands, .data$vertaling, .data$hash
-      ) %>%
-      summarise(laatste = max(.data$tijdstip), .groups = "drop") %>%
+    vragen |>
+      left_join(antwoord, by = "hash") -> ds
+    ds |>
+      group_by(across(head(colnames(ds), -3))) |>
+      summarise(laatste = max(.data$tijdstip), .groups = "drop") |>
       mutate(
         voorspelling = 1,
         laatste = replace_na(
@@ -28,45 +26,37 @@ selectiekans_bijwerken <- function(vragen, antwoord) {
       ) -> resultaat
     return(resultaat)
   }
-  vragen %>%
-    left_join(antwoord, by = "hash") %>%
+  stop("model")
+  vragen |>
+    left_join(antwoord, by = "hash") |>
     mutate(fout = 1 - .data$juist) -> dataset
-  formule <- "persoon"
-  if (length(levels(vragen$type)) > 1) {
-    stop("meer dan een type")
-    formule <- c(formule, "type")
-  }
-  if (length(levels(vragen$werkwoordstijd)) > 1) {
-    stop("meer dan een werkwoordstijd")
-    formule <- c(formule, "werkwoordstijd")
-  }
-  if (length(levels(vragen$werkwoord)) > 1) {
-    formule <- c(formule, "werkwoord")
-  }
-  formule <- sprintf(
-    paste(
-      "f(\n  %s, model = 'iid',",
+  dataset |>
+    select(where(is.factor)) |>
+    lapply(levels) -> covar
+  names(covar)[sapply(covar, length) > 1] |>
+    sprintf(
+      fmt = paste(
+        "f(\n  %s, model = 'iid',",
       "  hyper = list(theta = list(prior = 'pc.prec', param = c(3, 0.01)))\n)",
-      sep = "\n"
-    ),
-    formule
-  )
-  formule <- as.formula(paste("fout ~\n", paste(formule, collapse = " +\n")))
+        sep = "\n"
+      )
+    ) |>
+    paste(collapse = " +\n") |>
+    sprintf(fmt = "fout ~\n%s") |>
+    as.formula() -> formule
   model <- inla(
     formule, family = "binomial", data = dataset,
     control.predictor = list(link = 1)
   )
   dataset$voorspelling <- model$summary.fitted.values$`0.975quant`
-  dataset %>%
+  dataset |>
     group_by(
-      .data$type, .data$werkwoord, .data$persoon, .data$werkwoordstijd,
-      .data$nederlands, .data$vertaling, .data$hash
-    ) %>%
+      across(names(covar)), .data$vraag, .data$antwoord, .data$hash
+    ) |>
     summarise(
-      laatste = max(.data$tijdstip),
-      voorspelling = max(.data$voorspelling),
+      laatste = max(.data$tijdstip), voorspelling = max(.data$voorspelling),
       .groups = "drop"
-    ) %>%
+    ) |>
     mutate(
       laatste = replace_na(
         .data$laatste, min(.data$laatste, na.rm = TRUE) - 3600 * 24
@@ -76,6 +66,15 @@ selectiekans_bijwerken <- function(vragen, antwoord) {
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  fluidRow(
+    selectInput(
+      "taal", label = "Wat oefenen?", selected = "nederlands-frans",
+      choices = list(
+        "vervoegingen nederlands-frans" = "nederlands-frans",
+        "woorden latijn" = "latijn"
+      )
+    )
+  ),
   fluidRow(
     textOutput("vraag"),
     textInput("antwoord", label = "vertaling"),
@@ -91,25 +90,25 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(session, input, output) {
   data <- reactiveValues(
-    taal = "nederlands-frans", vertalingen = NULL, antwoorden = NULL,
+    vertalingen = NULL, antwoorden = NULL, type = "vervoeging",
     hash = NULL, vraag = NULL, correct = NULL, feedback = NULL
   )
-  observeEvent(data$taal, {
-    data$antwoorden <- lees_antwoorden(taal = data$taal)
-    data$vertalingen <- selectiekans_bijwerken(
-      lees_vertalingen(taal = data$taal), data$antwoorden
-    )
+  observeEvent(input$taal, {
+    data$antwoorden <- lees_antwoorden(taal = input$taal)
+    data$vertalingen <- lees_vertalingen(taal = input$taal) |>
+      selectiekans_bijwerken(data$antwoorden)
+    data$type <- ifelse(grepl(".+-.+", input$taal), "vervoeging", "woorden")
   })
   observeEvent(data$vertalingen, {
     if (is.null(data$vertalingen)) {
       return(NULL)
     }
-    data$vertalingen %>%
-      slice_min(.data$laatste, n = -5) %>%
+    data$vertalingen |>
+      slice_min(.data$laatste, n = -5) |>
       slice_sample(n = 1, weight_by = .data$voorspelling) -> vraag
     data$hash <- vraag$hash
-    data$vraag <- vraag$nederlands
-    data$correct <- vraag$vertaling
+    data$vraag <- vraag$vraag
+    data$correct <- vraag$antwoord
     output$vraag <- renderText({data$vraag})
   })
   observeEvent(input$controleer, {
@@ -137,13 +136,13 @@ server <- function(session, input, output) {
       )
     }
     data$antwoorden <- rbind(data$antwoorden, extra)
-    write_vc(data$antwoorden, data$taal, sorting = c("hash", "tijdstip"))
+    write_vc(data$antwoorden, input$taal, sorting = c("hash", "tijdstip"))
     vraag <- slice_sample(
       data$vertalingen, n = 1, weight_by = .data$voorspelling
     )
     data$hash <- vraag$hash
-    data$vraag <- vraag$nederlands
-    data$correct <- vraag$vertaling
+    data$vraag <- vraag$vraag
+    data$correct <- vraag$antwoord
     output$vraag <- renderText({data$vraag})
     updateTextInput(session = session, inputId = "antwoord", value = "")
   })
@@ -163,36 +162,43 @@ server <- function(session, input, output) {
       )
     }
 
-    output$inspanning <- renderPlot(
-      data$antwoorden %>%
+    output$inspanning <- renderPlot({
+      data$antwoorden |>
         filter(
           difftime(Sys.time(), data$antwoorden$tijdstip, units = "days") <= 28
-        ) %>%
+        ) -> ds
+      if (nrow(ds) == 0) {
+        return(NULL)
+      }
+      ds |>
         mutate(
           dag = trunc(.data$tijdstip, units = "days") %>%
             as.Date(),
           juist = factor(.data$juist, 0:1, c("fout", "juist"))
-        ) %>%
+        ) |>
         ggplot(aes(x = dag, fill = juist)) +
         geom_bar() +
         scale_x_date(date_labels = "%d %b") +
         theme(axis.title = element_blank()) +
         ggtitle("inspanning van de laatste vier weken")
-    )
+    })
 
-    output$inspanning_detail <- renderPlot(
-      data$antwoorden %>%
-        inner_join(data$vertalingen, by = "hash") %>%
+    output$inspanning_detail <- renderPlot({
+      if (nrow(data$antwoorden) == 0) {
+        return(NULL)
+      }
+      data$antwoorden |>
+        inner_join(data$vertalingen, by = "hash") |>
         mutate(
-          dag = round.POSIXt(.data$tijdstip, units = "day") %>%
+          dag = round.POSIXt(.data$tijdstip, units = "day") |>
             as.Date()
-        ) %>%
-        group_by(.data$werkwoord, .data$dag) %>%
-        summarise(aantal = n(), juist = mean(.data$juist), .groups = "drop") %>%
+        ) |>
+        group_by(.data$werkwoord, .data$dag) |>
+        summarise(aantal = n(), juist = mean(.data$juist), .groups = "drop") |>
         ggplot(aes(x = dag, y = juist, size = aantal)) + geom_point() +
         facet_wrap(~werkwoord) +
         scale_size_area()
-    )
+    })
   })
 
   session$onSessionEnded(function() {
--- helper.R
+++ helper.R
@@ -1,26 +1,43 @@
 lees_vertalingen <- function(
-    sheet = "1w85ll_qVEPkybDEPPovji_bmLAS5TXXFNqMRGOY0TTs",
-    taal = "nederlands-frans"
+  sheet = c(
+    vervoeging = "1w85ll_qVEPkybDEPPovji_bmLAS5TXXFNqMRGOY0TTs",
+    woorden = "1G6NBi9FE5wJnooKL1eIQyDCqFNUmWTi-SEyXF7PlEMQ"
+  ),
+  taal = "nederlands-frans"
 ) {
-  read_sheet(sheet, taal) %>%
-    mutate(
-      hash = pmap_chr(
-        list(
-          type = .data$type, werkwoord = .data$werkwoord,
-          persoon = .data$persoon, werkwoordstijd = .data$werkwoordstijd
+  if (grepl(".+-.+", taal)) {
+    read_sheet(sheet["vervoeging"], taal) |>
+      transmute(
+        hash = pmap_chr(
+          list(
+            type = .data$type, werkwoord = .data$werkwoord,
+            persoon = .data$persoon, werkwoordstijd = .data$werkwoordstijd
+          ),
+          ~sha1(list(...))
         ),
-        function(...) {
-          sha1(list(...))
-        }
-      ),
-      type = factor(.data$type),
-      werkwoord = factor(.data$werkwoord),
-      persoon = factor(.data$persoon),
-      werkwoordstijd = factor(.data$werkwoordstijd)
-    )
+        type = factor(.data$type), werkwoord = factor(.data$werkwoord),
+        persoon = factor(.data$persoon),
+        werkwoordstijd = factor(.data$werkwoordstijd),
+        vraag = sprintf("Geef de vertaling van `%s`", .data$nederlands),
+        antwoord = .data$vertaling
+      )
+  } else {
+    read_sheet(sheet["woorden"], taal) |>
+      transmute(
+        hash = pmap_chr(
+          list(type = .data$type, woord = .data$woord, kenmerk = .data$kenmerk),
+          ~sha1(list(...))
+        ),
+        .data$type, .data$kenmerk,
+        vraag = sprintf(
+          "Geef %s van `%s`", .data$woord, .data$kenmerk
+        ),
+        antwoord = .data$waarde
+      )
+  }
 }
 lees_antwoorden <- function(taal = "nederlands-frans") {
-  if(!is_git2rmeta(taal)) {
+  if (!is_git2rmeta(taal)) {
     return(
       data.frame(
         hash = character(0), tijdstip = as.POSIXct(integer(0)),
